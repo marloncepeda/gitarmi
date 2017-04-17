b920a310f279391cb17b9bec0a3ceb335395b19a
marlon cepeda <marloncepeda@tiendosqui.com>
2017-04-17 12:27:18 +0000

    reparado error donde faltaba el parentesis

--- shopkeepers/views.py
+++ shopkeepers/views.py
@@ -423,7 +423,7 @@ def mostSoldGlobally(request):
         try:
                 mostSold = extended_order.objects.all().annotate(image=F('product__product__picture'),price=F('product__base_price'),suggested_price=F('product__product__suggested_price'),name=F('product__product__name'),description=F('product__product__description')).values('product_id','image','price','suggested_price','name','description').annotate(total=Count('product_id')).order_by('-total')[:10]
                 if(len(mostSold)==0):
-                        return JsonResponse({'petition':'EMPTY','detail':'There are no products to calculate the best sellers'}
+                        return JsonResponse({'petition':'EMPTY','detail':'There are no products to calculate the best sellers'})
                 if(len(mostSold)==0):
                         return JsonResponse({'petition':'EMPTY','detail':'There are no products to calculate the best sellers'})
                 else:
@@ -442,4 +442,5 @@ def mostSoldGloballyHistory(request):
                         return Response(mostSold)
         except Exception as e:
                 return JsonResponse({"petition":"ERROR","detail":e.message})
+
 

c47f302a1e73c46733b99f6c4251ec9136f2eaaa
marlon cepeda <marloncepeda@tiendosqui.com>
2017-04-20 11:23:51 +0000

    agregado nuevo serializador de system

--- system/serializers.py
+++ system/serializers.py
@@ -5,3 +5,8 @@ from django.contrib.auth.models import User
 from .models import *
 from rest_framework import permissions
 from rest_framework.permissions import IsAdminUser
+
+class CompanySerializer(serializers.ModelSerializer):
+        class Meta:
+                model = types
+                fields = ('id','name','description','picture','address','phone1','phone2','phone3','phone4','email','date_register',)

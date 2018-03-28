ea0c026357eb544ac38c16bd7cbd8425d11611ee
marloncepeda <marlons.cepeda@gmail.com>
2018-03-28 09:39:07 -0500

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -10,10 +10,6 @@ class UsersTypeAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description',)
     list_display = ('name', 'description','date_register', )
 
-#class AddressAdmin(admin.ModelAdmin):
-#    search_fields = ('address_alias','address',)
-#    list_display = ('client', 'address_alias','city','address','address_detail','lat','lon','date_register')
-
 class UsersProfileAdmin(admin.ModelAdmin):
     search_fields = ('user', 'phone','type_user','status',)
     list_display = ('first_name','last_name','user_email', 'phone','type_user','status','date_register', )
@@ -27,18 +23,6 @@ class UsersProfileAdmin(admin.ModelAdmin):
     def last_name(self, instance):
     	return instance.user.last_name
 
-#class TagsAdmin(admin.ModelAdmin):
-#    search_fields = ('name', 'description',)
-#    list_display = ('name', 'description','date_register', )
-
-#class UsersTagsAdmin(admin.ModelAdmin):
-#    search_fields = ('user', 'tag',)
-#    list_display = ('user', 'tag','date_register', )
-
 admin.site.register(Status,StatusAdmin)
 admin.site.register(Types, UsersTypeAdmin)
 admin.site.register(Profile,UsersProfileAdmin)
-#admin.site.register(Address,AddressAdmin)
-#admin.site.register(tags,TagsAdmin)
-#admin.site.register(users_tags,UsersTagsAdmin)
-#"""
